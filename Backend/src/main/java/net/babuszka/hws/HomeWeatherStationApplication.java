7ed2ea17d77d412ad540073481e0c5b01362edbe
Kamil <kamil@osp.slawa.pl>
2020-12-15 21:48:55 +0100

    Init new Spring Project

---
+++ Backend/src/main/java/net/babuszka/hws/HomeWeatherStationApplication.java
@@ -0,0 +1,13 @@
+package net.babuszka.hws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWeatherStationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(net.babuszka.hws.HomeWeatherStationApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/net/babuszka/hws/HomeWeatherStationhwsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.babuszka.hws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWeatherStationhwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
